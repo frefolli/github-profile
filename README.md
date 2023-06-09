4996af9ad1a01a4390f00bdc5d2b239862214f0b
frefolli <f.refolli@campus.unimib.it>
2023-06-09 17:07:36 +0200

    fix

--- README.md
+++ README.md
@@ -1,6 +1,52 @@
+## languages
+
 ```mermaid
-  graph TD;
-    languages --> "C++";
-    languages --> "Java";
-    languages --> "Python";
-```
+mindmap
+	root(mindmap)
+    "Java"
+    "C++"
+    "C"
+    "Rust"
+    "Prolog"
+    "Lisp"
+    "Python"
+    "Javascript"
+    "Haskell"```
+
+## distros
+
+```mermaid
+mindmap
+	root(mindmap)
+    "Arch Linux"
+    "Fedora"
+    "Debian"
+    "Void"
+    "Slackware"
+    "Gentoo"```
+
+## editors
+
+```mermaid
+mindmap
+	root(mindmap)
+    "Vim"
+    "Neovim"
+    "Emacs"
+    "Notepad++"
+    "VS Code"```
+
+## technologies
+
+```mermaid
+mindmap
+	root(mindmap)
+    "Node JS"
+    "React JS"
+    "Docker"
+    "Java Spring"
+    "Facebook Proxygen"
+    "Make"
+    "CMake"
+    "Ninja"
+    "Meson"```
--- main.py
+++ main.py
@@ -11,16 +11,43 @@ def open_yaml_config(config_path: str):
             print(exc)
             exit(1)
 
+def write_profile(profile_path: str, text: str):
+    with open(profile_path, "w") as stream:
+        try:
+            stream.write(text)
+        except Exception as exc:
+            print(exc)
+            exit(1)
+
 def compose_cli() -> argparse.ArgumentParser:
     cli = argparse.ArgumentParser(description = "github profile generator")
     cli.add_argument("-i", "--input", default="config.yml", help="input yaml config file")
     cli.add_argument("-o", "--output", default="output.md", help="ouput MD profile file")
     return cli
 
+def forge_skill(skill_name, array):
+    text = "## %s\n\n" % skill_name
+    text += "```mermaid\nmindmap\n\troot(mindmap)"
+    for thing in array:
+        text += "\n    \"%s\"" % thing
+    text += "```"
+    return text
+
+def forge_skills(config):
+    if "skills" in config:
+        skills = config["skills"]
+        for skill in skills:
+            yield forge_skill(skill, skills[skill])
+
 def main():
-    cli_config = compose_cli().parse_args(sys.argv[1:])
-    profile_config = open_yaml_config(cli_config.input)
-    print(profile_config)
+    config = compose_cli().parse_args(sys.argv[1:])
+    profile = open_yaml_config(config.input)
+
+    sections = []
+    sections += list(forge_skills(profile))
+    text = "\n\n".join(sections)
+
+    write_profile(config.output, text)
 
 if __name__ == "__main__":
     main()
